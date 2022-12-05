cba4134e2388e1b1de826f7d4e350f7635e27f18
Cyber <Imambasha3944@gmail.com>
2022-12-05 11:11:26 +0530

    Adeed controller commment

--- src/main/java/com/org/minions/controller/MinionsController.java
+++ src/main/java/com/org/minions/controller/MinionsController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
-
+//Controller
 @RestController
 @RequestMapping("/api")
 public class MinionsController {
